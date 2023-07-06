cf19f3434b115e3fad26a17aea351613c1e099af
Usman <104586608+Usmandankama@users.noreply.github.com>
2023-07-06 11:21:19 +0100

    Second commit Sign_up_page edited

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -13,86 +13,96 @@ class _Signup_PageState extends State<Signup_Page> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Second page"),
-          ),
           body: Container(
-            padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
-            width: 500,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [
-                  Color.fromARGB(255, 1, 27, 143),
-                  Color.fromARGB(255, 9, 110, 141)
-                ],
+        padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+        width: 500,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color.fromARGB(255, 1, 27, 143),
+              Color.fromARGB(255, 9, 110, 141)
+            ],
+          ),
+        ),
+        child: Column(children: [
+          Column(
+            children: [
+              const SizedBox(
+                height: 150,
               ),
-            ),
-            child: Column(children: [
-              Image.asset('assets/icons/sign_up_avatar.png'),
-              Column(
-                children: [
-                  SizedBox(height: 5),
-                  Text("Full Name"),
-                  Container(
-                    margin: const EdgeInsets.only(top: 5, bottom: 5),
-                    padding: const EdgeInsets.only(left: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Username",
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 2),
-                  Text("Full Name",textAlign:TextAlign.start),
-                  Container(
-                    margin: const EdgeInsets.only(top: 5, bottom: 5),
-                    padding: const EdgeInsets.only(left: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Username",
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 2,width:5),
-                  Text("Full Name"),
-                  Container(
-                    padding: const EdgeInsets.only(left: 10),
-                    margin: const EdgeInsets.only(top: 5, bottom: 5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Password",
-                      ),
-                    ),
-                  ),
-                ],
+              const Text(
+                "Sign up",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-              ElevatedButton(
-                style: const ButtonStyle(
-                    ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Go back'),
+              const SizedBox(
+                height: 20,
               ),
-            ]),
-          )),
+              Container(
+                margin: const EdgeInsets.only(top: 5, bottom: 5),
+                padding: const EdgeInsets.only(left: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    border: InputBorder.none,
+                    hintText: "Username",
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5, bottom: 5),
+                padding: const EdgeInsets.only(left: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    border: InputBorder.none,
+                    hintText: "Enter Password",
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 5),
+                margin: const EdgeInsets.only(top: 5, bottom: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    border: InputBorder.none,
+                    hintText: "Confirm Password",
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.green),
+                padding: MaterialStatePropertyAll(
+                    EdgeInsets.only(left: 120, right: 120))),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back'),
+          ),
+        ]),
+      )),
     );
   }
 }
